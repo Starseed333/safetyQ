594aec2961b4835002fb73f21db16e865db6a441
Sandra Marquez <dnrvarela@yahoo.com>
2018-04-03 20:11:03 -0500

    html route

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+//html route using express
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
